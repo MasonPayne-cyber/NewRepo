f2d66951e1633bc5593aec1a56d087bad197e412
MasonPayne-cyber <mmccarthy7022@gmail.com>
2026-01-13 19:35:52 +0000

    3c

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 // input for searchbar to go here
-const searchinput = document.getElementById("searchoutput");
+const searchinput = document.getElementById("searchinput");
 const displayoutput = document.getElementById("displayoutput");
 searchinput.addEventListener("input" , (e) => {
 displayoutput.innerText = e.target.value;
